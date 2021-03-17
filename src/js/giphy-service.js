32781889a4c94140a29395521de9f5ca4acd5ab4
Cat-Denton <willwdenton@gmail.com>
2021-03-16 20:05:35 -0700

    add GiphyService class with getGiphy method in giphy-service.js file
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

---
+++ src/js/giphy-service.js
@@ -0,0 +1,17 @@
+export default class GiphyService {
+  static getGiphy(searchTerm) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}=${searchTerm}&limit=1&offset=0&rating=g&lang=en`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
