822765b0b2f02e157b14a5225c7014b25907d880
Cat-Denton <willwdenton@gmail.com>
2021-03-16 21:06:46 -0700

    add code to allow user to search for gif and display the first image returned
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import GiphyService from './js/giphy-service';
 
+// function clearFields() {
+//   $("#searchTerm").val("");
+//   $('.showErrors').text("");
+//   $('.showGif').text("");
+// }
 
+$("#giphyForm").submit(function() {
+  console.log("hi");
+  let searchTerm = $('#searchTerm').val();
+  // clearFields();
+  let promise = GiphyService.getGiphy(searchTerm);
+  promise.then(function(response) {
+    const body = JSON.parse(response);
+    let gifUrl = `${body.data[0].images.original.url}`;
+    let gifTitle = `${body.data[0].title}`;
+    console.log(gifUrl);
+    console.log(gifTitle);
+    $('#gifToShow').attr('src',gifUrl);
+    $('#gifToShow').attr('alt',gifTitle);
+  });
+  event.preventDefault();
+});
