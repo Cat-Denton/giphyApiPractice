7b93227fc2eac1b3a7b49326fdab7921e5031773
Cat-Denton <willwdenton@gmail.com>
2021-03-17 20:45:16 -0700

    increase number of images returned to 5
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- src/js/giphy-service.js
+++ src/js/giphy-service.js
@@ -2,7 +2,7 @@ export default class GiphyService {
   static getGiphy(searchTerm) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchTerm}&limit=1&offset=0&rating=g&lang=en`;
+      const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchTerm}&limit=5&offset=0&rating=g&lang=en`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
