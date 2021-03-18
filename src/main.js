44c85cd1d525f1c8ff38f7b5446aad400fd4bae1
Cat-Denton <willwdenton@gmail.com>
2021-03-17 19:33:55 -0700

    change to dynamically add output with displayGifs function
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,21 +6,26 @@ import GiphyService from './js/giphy-service';
 
 function clearFields() {
   $("#searchTerm").val("");
-  $('.showErrors').text("");
-  $('.showGif').attr("src", "");
-  $('.showGif').attr("alt", "");
+  $('#showErrors').text("");
+  $('#showGif').attr("src", "");
+  $('#showGif').attr("alt", "");
+}
+
+function displayGifs() {
+  let showGifDiv = $("div#showGif");
+  let htmlForGifDiv = `<img id='gifToShow'>`
+  showGifDiv.html(htmlForGifDiv);
 }
 
 $("#giphyForm").submit(function() {
   let searchTerm = $('#searchTerm').val();
   clearFields();
+  displayGifs();
   let promise = GiphyService.getGiphy(searchTerm);
   promise.then(function(response) {
     const body = JSON.parse(response);
     let gifUrl = `${body.data[0].images.original.url}`;
     let gifTitle = `${body.data[0].title}`;
-    console.log(gifUrl);
-    console.log(gifTitle);
     $('#gifToShow').attr('src',gifUrl);
     $('#gifToShow').attr('alt',gifTitle);
   });
